9a056480cb17db2f0b2ab8cf1cafcd57a09505b3
luantranminh <15520454@gm.uit.edu.vn>
2018-12-22 16:21:08 +0700

    Redirect to source link

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -18,32 +17,98 @@ import (
 
 var client *mongo.Client
 var config Config
+var err error
 
 const (
 	COLLECTION = "links"
 )
 
 // CreateEndpoint .
-func CreateEndpoint(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+func CreateEndpoint(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	var request MyURL
 
+	// decode request into MyURL struct
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		w.WriteHeader(401)
-		w.Write([]byte(err.Error()))
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
 		return
 	}
 
-}
+	collection := client.Database(config.Database).Collection(COLLECTION)
 
-// ExpandEndpoint .
-func ExpandEndpoint(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+	//Check if url already exists.
+	filter := bson.M{"url": request.URL}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var result MyURL
+		err := cur.Decode(&result)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		respondWithJSON(w, http.StatusOK, map[string]string{
+			"short_url": config.Hostname + result.ID,
+			"url":       result.URL,
+		})
+		return
+	}
+	if err := cur.Err(); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	// create new link
+	request.ID = CreateID()
+
+	ctx, c := context.WithTimeout(context.Background(), 60*time.Second)
+	_, err = collection.InsertOne(ctx, request)
+	defer c()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{
+		"short_url": config.Hostname + request.ID,
+		"url":       request.URL,
+	})
 }
 
 // Root .
-func Root(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Hello and welcome to the url shortener service")
+func Root(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+	var request MyURL
+
+	request.ID = params.ByName("id")
+
+	collection := client.Database(config.Database).Collection(COLLECTION)
+
+	//Check if url already exists.
+	filter := bson.M{"_id": request.ID}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cur, err := collection.Find(ctx, filter)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	defer cur.Close(ctx)
+
+	for cur.Next(ctx) {
+		var result MyURL
+		err := cur.Decode(&result)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, err.Error())
+			return
+		}
+		http.Redirect(w, r, result.URL, 301)
+		return
+	}
 }
 
 // CreateID creates new ID by hashid with timestamp.
@@ -57,33 +122,33 @@ func CreateID() string {
 	return id
 }
 
-func main() {
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+func init() {
 	config.Read()
+}
+
+func main() {
 
 	// connect to mongoDB
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(ctx, config.Server)
+	client, err = mongo.Connect(ctx, config.Server)
 	if err != nil {
 		log.Fatal("Cannot connect to mongoDB by error", err)
 	}
 	defer cancel()
 
-	collection := client.Database(config.Database).Collection(COLLECTION)
-	ctx, c := context.WithTimeout(context.Background(), 10*time.Second)
-	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159, "_id": "asdsax"})
-	defer c()
-	fmt.Println(bson.RawValue{})
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	id := res.InsertedID
-	fmt.Print(id)
-
 	router := httprouter.New()
 	router.POST("/create", CreateEndpoint)
-	router.GET("/:id", ExpandEndpoint)
-	router.GET("/", Root)
+	router.GET("/:id/", Root)
 
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
--- models/url.go
+++ models/url.go
@@ -2,7 +2,6 @@ package models
 
 // MyURL the properties in mongodb document
 type MyURL struct {
-	ID       string `bson:"_id" json:"id"`
-	ShortURL string `bson:"short_url" json:"short_url"`
-	LongURL  string `bson:"long_url" json:"long_url"`
+	ID  string `bson:"_id" json:"id"`
+	URL string `bson:"url" json:"url"`
 }
